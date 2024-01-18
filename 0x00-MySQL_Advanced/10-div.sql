30e0c8f0071190c89167f1125f63173b95d2912e
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-18 18:01:25 +0000

    Safe div function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- Create a function
+DELIMITER &&
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+BEGIN
+	DECLARE result FLOAT DEFAULT 0;
+	IF b != 0 THEN
+		SET result = a / b;
+	END IF;
+	RETURN result;
+END &&
+DELIMITER ;
