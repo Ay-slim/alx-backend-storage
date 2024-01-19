51c943a61cd9f9eb33dd1a86c53be9886d4ad59a
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-19 08:06:36 +0000

    Use last meeting condition appropriately

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,2 @@
 -- View SQL statement
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE students.score < 80 OR DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH);
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE students.score < 80 OR last_meeting < DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH);
